d67ca0a45dd33b36bca4db1eefb46fb43dd8f614
Vedrán Krisztián <vedran.krisztian@burg.lo.hu>
2024-09-05 09:16:27 +0200

    general diveket

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-"use strict"
+function generateDivs() {
+    let container = document.getElementById('container');
+    container.innerHTML = '';
+
+    let napokSzama = document.getElementById('honapok').value;
+
+    
+        for (let i = 1; i <= napokSzama; i++) {
+            let rowDiv = document.createElement('div');
+
+            let dayNumber = document.createElement('span');
+            dayNumber.textContent = i + ". ";
+
+            let input = document.createElement('input');
+            input.type = 'text';
+
+            rowDiv.appendChild(dayNumber);
+            rowDiv.appendChild(input);
+
+            container.appendChild(rowDiv);
+        }
+    
+}
